498c41412261928876a7b53412980e25303abe47
revun <romik.rev@gmail.com>
2023-03-29 14:20:53 +0300

    implement npm publis

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    commonjs: true,
+    jquery: true,
+    node: true,
+  },
+  plugins: ["prettier"],
+  extends: ["airbnb", "eslint:recommended", "plugin:prettier/recommended"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: "module",
+  },
+  rules: {
+    semi: "error",
+    indent: "off",
+    sub: "off",
+    camelcase: "off",
+    "no-console": "error",
+  },
+};
