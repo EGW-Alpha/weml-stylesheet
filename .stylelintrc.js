7e25edbc6c9eee45bd045c56912cf670d3907675
revun <romik.rev@gmail.com>
2023-03-29 14:20:53 +0300

    implement npm publis

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,10 +1,17 @@
 module.exports = {
-  extends: ['stylelint-config-recommended', 'stylelint-config-recommended-scss', 'stylelint-config-html', 'stylelint-config-sass-guidelines'],
-  plugins: ['stylelint-scss'],
+  extends: [
+    "stylelint-config-recommended",
+    "stylelint-config-recommended-scss",
+    "stylelint-config-html",
+    "stylelint-config-sass-guidelines",
+  ],
+  plugins: ["stylelint-scss"],
   rules: {
-    'selector-class-pattern': null,
-    'selector-no-qualifying-type': [ true, { ignore: ["attribute", "class", "id"] }],
-    'max-nesting-depth': 5,
+    "selector-class-pattern": null,
+    "selector-no-qualifying-type": [
+      true,
+      { ignore: ["attribute", "class", "id"] },
+    ],
+    "max-nesting-depth": 5,
   },
-
 };
